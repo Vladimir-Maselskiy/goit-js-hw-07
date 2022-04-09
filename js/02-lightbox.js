f75f58329bbda6d43ce88c33b60116e939ad5339
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-04-09 22:53:37 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDivRef = document.querySelector(".gallery");
+
+const createImageGrid = () => {
+	const markup = galleryItems
+		.map((item) => {
+			const { preview: smallImage, original: largeImage, description } = item;
+			return `
+        <a onclick="return false" class="gallery__link" href="${largeImage}">
+            <img
+                class="gallery__image"
+                src="${smallImage}"
+                alt="${description}"
+                captionSelector = "1000"
+
+            />
+        </a>
+    `;
+		})
+		.join("");
+	return markup;
+};
+
+galleryDivRef.innerHTML = createImageGrid();
+var gallery = new SimpleLightbox(".gallery a");
+gallery.options.captionsData = "alt";
+gallery.options.captionDelay = 250;
